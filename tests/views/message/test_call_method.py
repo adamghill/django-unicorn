62f738c5dce0ac42678fe2780c3fc50c4ccc89cb
Adam Hill <ahill@fool.com>
2020-10-25 15:39:17 -0400

    Clean up test.

--- tests/views/message/test_call_method.py
+++ tests/views/message/test_call_method.py
@@ -7,28 +7,6 @@ def test_message_call_method(client):
     data = {}
     message = {
         "actionQueue": [
-            {"payload": {"name": "refresh", "params": []}, "type": "callMethod",}
-        ],
-        "data": data,
-        "checksum": generate_checksum(orjson.dumps(data)),
-        "id": "FDHcbzGf",
-    }
-
-    response = client.post(
-        "/message/tests.views.fake_components.TestComponent",
-        message,
-        content_type="application/json",
-    )
-
-    body = orjson.loads(response.content)
-
-    assert body["data"].get("dictionary") == {"name": "test"}
-
-
-def test_message_call_method(client):
-    data = {}
-    message = {
-        "actionQueue": [
             {"payload": {"name": "test_method", "params": []}, "type": "callMethod",}
         ],
         "data": data,
