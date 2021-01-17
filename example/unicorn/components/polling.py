1df4d9f9384beee355549f6c4dda78807ea43b1e
Adam Hill <adamghill@gmail.com>
2021-01-17 16:23:59 -0500

    Add example of Django messages.

--- example/unicorn/components/polling.py
+++ example/unicorn/components/polling.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.utils.timezone import now
 
 from django_unicorn.components import PollUpdate, UnicornView
@@ -12,10 +13,14 @@ class PollingView(UnicornView):
         self.current_time = now()
         self.date_example = now()
 
+        messages.error(self.request, "get_date called :(")
+
         return PollUpdate(timing=2000, disable=False, method="get_date_2")
 
     def get_date_2(self):
         self.current_time = now()
         self.date_example = now()
+
+        messages.success(self.request, "get_date2 called :)")
 
         return PollUpdate(timing=1000, disable=False, method="get_date")
