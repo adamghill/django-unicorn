130d149f8be6617956b53b312d65ea365c50d7bd
Fredrik Borg <fredrikb@mnemonic.no>
2021-02-02 20:02:13 +0100

    use ast for parsing

--- django_unicorn/call_method_parser.py
+++ django_unicorn/call_method_parser.py
@@ -1,5 +1,5 @@
+import ast
 import logging
-from ast import literal_eval
 from typing import Any, Dict, List, Tuple
 from uuid import UUID
 
@@ -22,21 +22,32 @@ CASTERS = [
     lambda a: UUID(a),
 ]
 
-STOP_CHARACTERS = [":", ",", "]", "}", ")"]
-STRING_CHARACTERS = ["'", '"']
-
 
 class InvalidKwarg(Exception):
     pass
 
 
+def eval_arg(arg):
+    try:
+        arg = ast.literal_eval(arg)
+    except SyntaxError:
+        for caster in CASTERS:
+            try:
+                casted_value = caster(arg)
+
+                if casted_value:
+                    arg = casted_value
+                    break
+            except ValueError:
+                pass
+
+    return arg
+
+
 def parse_kwarg(kwarg: str, raise_if_unparseable=False) -> Dict[str, Any]:
     """
     Parses a potential kwarg as a string into a dictionary.
-
     For example: `parse_kwarg("test='1'")` == `{"test": "1"}`
-
-
     """
 
     parsed_kwarg = {}
@@ -70,7 +81,7 @@ def parse_kwarg(kwarg: str, raise_if_unparseable=False) -> Dict[str, Any]:
     # (the value can be a template variable that will get set from the context when
     # the templatetag is rendered in which case it can't be parsed in this manner)
     try:
-        val = parse_args(val)[0]
+        val = eval_arg(val)
     except ValueError:
         if raise_if_unparseable:
             raise
@@ -78,108 +89,6 @@ def parse_kwarg(kwarg: str, raise_if_unparseable=False) -> Dict[str, Any]:
     parsed_kwarg[key] = val
 
     return parsed_kwarg
-
-
-def parse_args(args: str) -> List[Any]:
-    """
-    Parses arguments as a string into Python objects.
-
-    For example: "'1'" would result in a "1". Or "[1, 2]" would result in [1, 2].
-
-    Handles strings, ints, floats, lists, tuples, dictionaries, sets, datetime, UUID.
-
-    Datetime is parsed with `parse_datetime`: https://docs.djangoproject.com/en/stable/ref/utils/#django.utils.dateparse.parse_datetime.
-    """
-    found_args = []
-    arg = ""
-
-    curly_bracket_count = 0
-    square_bracket_count = 0
-    parenthesis_count = 0
-
-    inside_string = False
-
-    def _eval_arg(_arg):
-        try:
-            _arg = literal_eval(_arg)
-        except SyntaxError:
-            for caster in CASTERS:
-                try:
-                    casted_value = caster(_arg)
-
-                    if casted_value:
-                        _arg = casted_value
-                        break
-                except ValueError:
-                    pass
-
-        return _arg
-
-    def _parse_arg(_arg):
-        if (
-            curly_bracket_count == 0
-            and square_bracket_count == 0
-            and parenthesis_count == 0
-        ):
-            _arg = _eval_arg(_arg)
-            found_args.append(_arg)
-            return ""
-
-        return _arg
-
-    for c in args:
-        if len(arg) > 1:
-            if c in STOP_CHARACTERS:
-                previous_char = arg[-1:][0]
-
-                if previous_char == "'":
-                    inside_string = False
-
-        if not inside_string and not c.strip():
-            continue
-
-        if (
-            c == ","
-            and not inside_string
-            and curly_bracket_count == 0
-            and square_bracket_count == 0
-            and parenthesis_count == 0
-        ):
-            if arg:
-                arg = _eval_arg(arg)
-                found_args.append(arg)
-                arg = ""
-
-            continue
-
-        arg += c
-
-        if c == "{":
-            curly_bracket_count += 1
-        elif c == "}":
-            curly_bracket_count -= 1
-            arg = _parse_arg(arg)
-        elif c == "[":
-            square_bracket_count += 1
-        elif c == "]":
-            square_bracket_count -= 1
-            arg = _parse_arg(arg)
-        elif c == "(":
-            parenthesis_count += 1
-        elif c == ")":
-            parenthesis_count -= 1
-            arg = _parse_arg(arg)
-        elif c in STRING_CHARACTERS:
-            inside_string = True
-
-    if arg:
-        if arg.startswith("'") and arg.endswith("'") and len(arg.split("'")) > 3:
-            arg = arg[1:-1]
-
-        arg = _eval_arg(arg)
-        found_args.append(arg)
-
-    return found_args
 
 
 def parse_call_method_name(call_method_name: str) -> Tuple[str, List[Any]]:
@@ -193,24 +102,28 @@ def parse_call_method_name(call_method_name: str) -> Tuple[str, List[Any]]:
         Tuple of method_name and a list of arguments.
     """
 
+    # Special function with "$" - remove before parsing with AST
+    if call_method_name.startswith("$"):
+        dollar_func = True
+        call_method_name = call_method_name[1:]
+    else:
+        dollar_func = False
+
+    tree = ast.parse(call_method_name, "eval")
     method_name = call_method_name
     params: List[Any] = []
+    kwargs: Dict[str, Any] = {}
 
-    if "(" in call_method_name and call_method_name.endswith(")"):
-        param_idx = call_method_name.index("(")
-        params_str = call_method_name[param_idx:]
+    if tree.body and isinstance(tree.body[0].value, ast.Call):
+        call = tree.body[0].value
+        method_name = call.func.id
+        params = [eval_arg(arg) for arg in call.args]
 
-        # Remove the arguments from the method name
-        method_name = call_method_name.replace(params_str, "")
+        # Not returned, but might be usable
+        kwargs = {kw.arg: eval_arg(kw.value) for kw in call.keywords}
 
-        # Remove parenthesis
-        params_str = params_str[1:-1]
-
-        if params_str == "":
-            return (method_name, params)
-
-        # Split up mutiple args
-        # TODO: Handle kwargs
-        params = parse_args(params_str)
+    # Insert "$" if special function
+    if dollar_func:
+        method_name = "$" + method_name
 
     return (method_name, params)
--- tests/call_method_parser/test_parse_call_method_name.py
+++ tests/call_method_parser/test_parse_call_method_name.py
@@ -30,8 +30,15 @@ def test_multiple_args():
     assert actual == expected
 
 
+def test_var_with_curly_braces():
+    expected = ("set_name", ["{}", ])
+    actual = parse_call_method_name('set_name("{}")')
+
+    assert actual == expected
+
+
 def test_one_arg():
-    expected = ("set_name", ["1",])
+    expected = ("set_name", ["1", ])
     actual = parse_call_method_name('set_name("1")')
 
     assert actual == expected
