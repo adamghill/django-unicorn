2e4726040dcaf5ea2f45b43d888bbc57a7ff4125
Adam Hill <adamghill@gmail.com>
2021-02-24 22:11:27 -0500

    Use `nox` to test against Django 2 and 3.

---
+++ noxfile.py
@@ -0,0 +1,11 @@
+import nox
+
+
+@nox.session()
+@nox.parametrize("django", ["2.2", "3.0"])
+def tests(session, django):
+    session.install("poetry")
+    session.run("poetry", "install")
+    session.install(f"django=={django}")
+    session.run("pytest", "-m", "not slow")
+    session.run("pytest", "-m", "slow")
