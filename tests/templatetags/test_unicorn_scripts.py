d622d953c9b6ef6271f5cf7e675c4b1df38f36d5
Adam Hill <adamghill@gmail.com>
2021-03-09 21:21:07 -0500

    Handle custom CSRF header name. Fixes https://github.com/adamghill/django-unicorn/issues/14.

---
+++ tests/templatetags/test_unicorn_scripts.py
@@ -0,0 +1,38 @@
+from django_unicorn.templatetags.unicorn import unicorn_scripts
+
+
+def test_unicorn_scripts():
+    actual = unicorn_scripts()
+
+    assert actual["CSRF_HEADER_NAME"] == "X-CSRFTOKEN"
+    assert actual["MINIFIED"] is True
+
+
+def test_unicorn_scripts_debug(settings):
+    settings.DEBUG = True
+    actual = unicorn_scripts()
+
+    assert actual["CSRF_HEADER_NAME"] == "X-CSRFTOKEN"
+    assert actual["MINIFIED"] is False
+
+
+def test_unicorn_scripts_minified_true(settings):
+    settings.UNICORN = {"MINIFIED": True}
+    actual = unicorn_scripts()
+
+    assert actual["CSRF_HEADER_NAME"] == "X-CSRFTOKEN"
+    assert actual["MINIFIED"] is True
+
+
+def test_unicorn_scripts_minified_false(settings):
+    settings.UNICORN = {"MINIFIED": False}
+    actual = unicorn_scripts()
+
+    assert actual["MINIFIED"] is False
+
+
+def test_unicorn_scripts_csrf_header_name(settings):
+    settings.CSRF_HEADER_NAME = "HTTP_X_UNICORN"
+    actual = unicorn_scripts()
+
+    assert actual["CSRF_HEADER_NAME"] == "X-UNICORN"
