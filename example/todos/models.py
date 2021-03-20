4941d4155ba390d4e59516e2527209bf44684aac
Adam Hill <adamghill@gmail.com>
2021-03-20 11:44:41 -0400

    Remove issue-168 example because the models example demonstrates the same functionality now.

--- example/todos/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Todo(models.Model):
-    description = models.CharField(max_length=50)
-    is_completed = models.BooleanField(default=False, blank=True)
-    due_date = models.DateField(null=True, blank=True)
