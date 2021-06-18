d2e48a6e7b1506b8c54c687ec41e09349705066e
Adam Hill <adamghill@gmail.com>
2021-06-17 21:20:37 -0400

    Add email validation example.

--- example/unicorn/forms.py
+++ example/unicorn/forms.py
@@ -5,3 +5,4 @@ class ValidationForm(forms.Form):
     text = forms.CharField(min_length=3, max_length=10)
     date_time = forms.DateTimeField()
     number = forms.IntegerField()
+    email = forms.EmailField()
