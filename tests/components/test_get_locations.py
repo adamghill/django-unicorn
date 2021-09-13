cbfc1a04b9db79f6af9035981df8d25965c74c3b
Apoorva Pandey <apoorva@datapane.com>
2021-09-13 13:53:01 +0530

    Use rindex when handling installed app that points to an app config

--- tests/components/test_get_locations.py
+++ tests/components/test_get_locations.py
@@ -29,7 +29,6 @@ def test_get_locations_fully_qualified_with_dots():
         ("HelloWorldView", "project.components.hello_world"),
     ]
     actual = get_locations("project.components.hello_world.HelloWorldView")
-    print(actual)
 
     assert expected == actual
 
@@ -118,10 +117,17 @@ def test_get_locations_apps_setting_invalid(settings):
 def test_get_locations_installed_app_with_app_config(settings):
     unicorn_apps = settings.UNICORN["APPS"]
     del settings.UNICORN["APPS"]
-    settings.INSTALLED_APPS = ("example.coffee.apps.Config",)
+    settings.INSTALLED_APPS = ["example.coffee.apps.Config",]
 
     expected = [("HelloWorldView", "example.coffee.components.hello_world",)]
     actual = get_locations("hello-world")
 
     assert expected == actual
+
+    # test when the app is in a subdirectory "apps"
+    settings.INSTALLED_APPS[0] = "foo_project.apps.bar_app.apps.Config"
+    expected_location = [("FooBarView", "foo_project.apps.bar_app.components.foo_bar")]
+    actual_location = get_locations("foo-bar")
+    assert expected_location == actual_location
+
     settings.UNICORN["APPS"] = unicorn_apps
