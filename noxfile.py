b27acbe58047a04d829a9e8d77027bbfae02c2c1
adamghill <adamghill@yahoo.com>
2021-12-28 22:23:06 -0500

    Install minify extra in nox.

--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ import nox
 @nox.parametrize("django", ["2.2", "3.0", "3.2"])
 def tests(session, django):
     session.install("poetry")
-    session.run("poetry", "install")
+    session.run("poetry", "install", "-E", "minify")
     session.install(f"django=={django}")
     session.run("pytest", "-m", "not slow")
     session.run("pytest", "-m", "slow")
