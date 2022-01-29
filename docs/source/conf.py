154fa4558213fa5042a070f842181c6f894260fb
adamghill <adamghill@yahoo.com>
2022-01-29 13:33:57 -0500

    Add basic Sphinx config file.

---
+++ docs/source/conf.py
@@ -0,0 +1,84 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+
+
+# sys.path.insert(0, os.path.abspath("....django-unicorn"))
+
+
+# -- Project information -----------------------------------------------------
+
+project = "Unicorn"
+copyright = "2021, Adam Hill"
+author = "Adam Hill"
+
+# The full version, including alpha/beta/rc tags
+release = "0.42.0"
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
+    "myst_parser",
+    "sphinx_copybutton",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosectionlabel",
+    "rst2pdf.pdfbuilder",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+}
+intersphinx_disabled_domains = ["std"]
+
+autosectionlabel_prefix_document = True
+autosectionlabel_maxdepth = 3
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "furo"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = []
+html_title = "Unicorn"
+
+myst_heading_anchors = 3
+myst_enable_extensions = ["linkify", "colon_fence"]
+
+pdf_documents = [
+    ("index", "unicorn-latest", "Unicorn", "Adam Hill"),
+]
