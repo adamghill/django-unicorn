39279f2880e22850aa2492cbb968d1c70221d8e4
Christian González <christian.gonzalez@nerdocs.at>
2022-03-12 18:41:23 +0100

    add a forms test

--- tests/urls.py
+++ tests/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+from tests.views.test_unicorn_forms import SimpleFormView
 
 urlpatterns = (
     path("test", TemplateView.as_view(template_name="templates/test_template.html")),
+    path("test_forms", SimpleFormView.as_view()),
     path("", include("django_unicorn.urls")),
 )
