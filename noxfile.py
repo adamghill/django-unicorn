fa82cf6e77300acb27903c044863a7a206b2002e
adamghill <adamghill@yahoo.com>
2022-11-16 08:40:01 -0500

    Add Django 4.1 to test matrix.

--- noxfile.py
+++ noxfile.py
@@ -4,10 +4,14 @@ import nox
 
 
 @nox.session()
-@nox.parametrize("django", ["2.2", "3.2", "4.0"])
+@nox.parametrize("django", ["2.2", "3.2", "4.0", "4.1"])
 def tests(session, django):
     # Skip testing Django 4.0 with Python 3.7 because it is unsupported
-    if django == "4.0" and sys.version_info.major == 3 and sys.version_info.minor == 7:
+    if (
+        django.startswith("4.")
+        and sys.version_info.major == 3
+        and sys.version_info.minor == 7
+    ):
         return
 
     session.install("poetry")
