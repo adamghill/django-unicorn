1c4ca3e83d4973467315f23d352a29188f914646
adamghill <adamghill@yahoo.com>
2022-11-16 08:26:26 -0500

    Skip testing Django 4.0 with Python 3.7 because it is unsupported.

--- noxfile.py
+++ noxfile.py
@@ -1,9 +1,15 @@
+import sys
+
 import nox
 
 
 @nox.session()
 @nox.parametrize("django", ["2.2", "3.2", "4.0"])
 def tests(session, django):
+    # Skip testing Django 4.0 with Python 3.7 because it is unsupported
+    if django == "4.0" and sys.version_info.major == 3 and sys.version_info.minor == 7:
+        return
+
     session.install("poetry")
     session.run("poetry", "install", "-E", "minify")
     session.install(f"django=={django}")
