584f2bb16a3e73ed0176200f5ccff1cb7ce70727
Felps <felipmartins94@gmail.com>
2024-01-05 14:22:13 -0300

    replaces loop code for extracted methods calls

--- django_unicorn/management/commands/startunicorn.py
+++ django_unicorn/management/commands/startunicorn.py
@@ -210,111 +210,21 @@ class Command(BaseCommand):
                     f"An app named '{app_name}' does not exist yet. You might need to create it first."
                 ) from e
 
-        is_first_component = False
-
         if not app_directory.exists():
             app_directory.mkdir()
 
-        # Create component
-        component_base_path = app_directory / "components"
+        paths, is_first = self.check_initials_directories(app_directory)
 
-        if not component_base_path.exists():
-            component_base_path.mkdir()
+        for name in options["component_names"]:
+            nested_path, name = self.obtain_nested_path(name)
 
-            self.stdout.write(self.style.SUCCESS(f"Created your first component in '{app_name}'! ✨\n"))
+            self.create_nested_directories(paths, nested_path)
 
-            is_first_component = True
+            self.create_component_and_template(paths, nested_path, name)
 
-        (component_base_path / "__init__.py").touch(exist_ok=True)
-
-        nested_paths = []
-
-        for component_name in options["component_names"]:
-            if "." in component_name:
-                (*nested_paths, component_name) = component_name.split(".")  # noqa: PLW2901
-
-                for nested_path in nested_paths:
-                    component_base_path /= nested_path
-
-                    if not component_base_path.exists():
-                        component_base_path.mkdir()
-
-                    (component_base_path / "__init__.py").touch(exist_ok=True)
-
-            snake_case_component_name = convert_to_snake_case(component_name)
-            pascal_case_component_name = convert_to_pascal_case(component_name)
-
-            component_path = component_base_path / f"{snake_case_component_name}.py"
-
-            if component_path.exists():
-                self.stdout.write(
-                    self.style.ERROR(f"Skipping creating {snake_case_component_name}.py because it already exists.")
-                )
-            else:
-                component_path.write_text(
-                    COMPONENT_FILE_CONTENT.format(**{"pascal_case_component_name": pascal_case_component_name})
-                )
-                self.stdout.write(self.style.SUCCESS(f"Created {component_path}."))
-
-            # Create template
-            template_base_path = app_directory / "templates" / "unicorn"
-
-            if not template_base_path.exists():
-                if not (app_directory / "templates").exists():
-                    (app_directory / "templates").mkdir()
-
-                template_base_path.mkdir()
-
-            for nested_path in nested_paths:
-                template_base_path /= nested_path
-
-                if not template_base_path.exists():
-                    template_base_path.mkdir()
-
-            template_path = template_base_path / f"{component_name}.html"
-
-            if template_path.exists():
-                self.stdout.write(
-                    self.style.ERROR(f"Skipping creating {component_name}.html because it already exists.")
-                )
-            else:
-                template_path.write_text(TEMPLATE_FILE_CONTENT)
-                self.stdout.write(self.style.SUCCESS(f"Created {template_path}."))
-
-            if is_first_component:
-                will_star_repo = input(
-                    "\nStarring the GitHub repo helps other Django users find Unicorn. Can you star it for me? [y/N] "
-                )
-
-                if will_star_repo.strip().lower() in ("y", "yes"):
-                    self.stdout.write(self.style.SUCCESS("Thank you for helping spread the word about Unicorn!"))
-
-                    self.stdout.write(
-                        """
-                         ,/
-                        //
-                      ,//
-          __    /|   |//
-      `__/\\_ --(/|___/-/
-   \\|\\_-\\___ __-_`- /-/ \\.
-  |\\_-___,-\\_____--/_)' ) \\
-   \\ -_ /     __ \\( `( __`\\|
-   `\\__|      |\\)      (/|
-',--//-|      \\    | '   /
-  /,---|       \\        /
- `_/ _,'        |      |
- __/'/          |      |
- ___/           \\ (  ) /
-                 \\____/\\
-                        \\
-"""
-                    )
-
-                    webbrowser.open("https://github.com/adamghill/django-unicorn", new=2)
-                else:
-                    self.stdout.write(
-                        self.style.ERROR("That's a bummer, but I understand. I hope you will star it for me later!")
-                    )
+            if is_first:
+                self.ask_for_star_repo()
+                is_first = False
 
             if is_new_app:
                 msg = f'\nMake sure to add `"{app_name}",` to your INSTALLED_APPS list in your settings file if necessary.'  # noqa: E501
