587f688752b2e8c0a6e782b0d795f17a58e642f7
adamghill <adamghill@yahoo.com>
2024-07-28 10:51:05 -0400

    Remove noxfile.

--- noxfile.py
+++
@@ -1,9 +0,0 @@
-import nox
-
-
-@nox.session()
-def tests(session):
-    session.install("poetry")
-    session.run("poetry", "install", "-E", "minify")
-    session.run("pytest", "-m", "not slow")
-    session.run("pytest", "-m", "slow")
