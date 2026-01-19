5fed6984e786b4b5d158e3cb931a3764b0dd03a2
adamghill <adamghill@yahoo.com>
2026-01-18 20:03:00 -0500

    Fix lints.

--- src/django_unicorn/typer.py
+++ src/django_unicorn/typer.py
@@ -98,9 +98,11 @@ def get_type_hints(obj) -> dict:
         if inspect.isclass(obj) or inspect.ismodule(obj) or inspect.isroutine(obj):
             type_hints = typing_get_type_hints(obj)
         elif hasattr(obj, "__class__"):
-            # Should be called with class object (instead of instance) to get type hints of parent classes. From docs:
-            # "If obj is a class C, the function returns a dictionary that merges annotations from C’s base classes with those on C directly.
-            # This is done by traversing C.__mro__ and iteratively combining __annotations__ dictionaries." (https://docs.python.org/3/library/typing.html#typing.get_type_hints)
+            # Should be called with class object (instead of instance) to get type hints of parent classes. from docs:
+            # "If obj is a class C, the function returns a dictionary that merges annotations from C's base classes with
+            # those on C directly. This is done by traversing C.__mro__ and iteratively combining __annotations__
+            # dictionaries."
+            # (https://docs.python.org/3/library/typing.html#typing.get_type_hints)
             type_hints = typing_get_type_hints(obj.__class__)
         else:
             type_hints = typing_get_type_hints(obj)
