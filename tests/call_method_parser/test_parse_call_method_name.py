64efa369567af59497cb1e9ca58a723e6bd7d201
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2026-02-11 12:53:23 +0000

    Fix: Properly escape single quotes in Unicorn.call() arguments

--- tests/call_method_parser/test_parse_call_method_name.py
+++ tests/call_method_parser/test_parse_call_method_name.py
@@ -82,3 +82,44 @@ def test_special_method_with_parens():
     actual = parse_call_method_name("$reset()")
 
     assert actual == expected
+
+
+def test_string_with_single_quotes():
+    expected = ("set_name", ("It's a test",), {})
+    actual = parse_call_method_name("set_name('It\\'s a test')")
+
+    assert actual == expected
+
+
+def test_string_with_double_quotes():
+    expected = ("set_name", ('He said "hello"',), {})
+    actual = parse_call_method_name('set_name(\'He said "hello"\')')
+
+    assert actual == expected
+
+
+def test_string_with_backslashes():
+    expected = ("set_name", ("C:\\Users\\test",), {})
+    actual = parse_call_method_name("set_name('C:\\\\Users\\\\test')")
+
+    assert actual == expected
+
+
+def test_complex_chemical_name_issue_607():
+    """Test for issue #607: string values with single quotes break function calls"""
+    chemical_name = "Chloro(2-dicyclohexylphosphino-3,6-dimethoxy-2',4',6'-tri-i-propyl-1,1'-biphenyl)(2'-amino-1,1'-biphenyl-2-yl)palladium(II)"
+    expected = ("set_new_catalyst", (chemical_name,), {})
+    actual = parse_call_method_name(
+        "set_new_catalyst('Chloro(2-dicyclohexylphosphino-3,6-dimethoxy-2\\',"
+        "4\\',6\\'-tri-i-propyl-1,1\\'-biphenyl)(2\\'-amino-1,1\\'-biphenyl-2-yl)palladium(II)')"
+    )
+
+    assert actual == expected
+
+
+def test_string_with_mixed_quotes_and_parentheses():
+    expected = ("set_name", ("test(with 'quotes' and \"double\")",), {})
+    actual = parse_call_method_name("set_name('test(with \\'quotes\\' and \"double\")')")
+
+    assert actual == expected
+
